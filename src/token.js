2a8b2c009e2a80ed8b5ba0d233010c817ea86c60
Vladislav Ivanov <103201559+DesperatePerson@users.noreply.github.com>
2023-12-01 14:47:10 +0500

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,15 +1,8 @@
 window.onload = () => {
-  YaAuthSuggest.init(
-      {
-         client_id: 'c46f0c53093440c39f12eff95a9f2f93',
-         response_type: 'token',
-         redirect_uri: 'https://examplesite.com/suggest/token'
-      },
-      'https://examplesite.com'
-   )
-   .then(({
-      handler
-   }) => handler())
-   .then(data => console.log('Сообщение с токеном', data))
-   .catch(error => console.log('Обработка ошибки', error));
+  YaSendSuggestToken(
+    'https://oauth-master-class-azure.vercel.app/', 
+    {
+      flag: true
+    }
+  )
 };
