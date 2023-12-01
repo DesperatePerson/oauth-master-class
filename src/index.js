4310e4b9aaa9b98ebdc79f0e45245440cdbcd3e1
Vladislav Ivanov <103201559+DesperatePerson@users.noreply.github.com>
2023-12-01 21:07:55 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,12 +33,12 @@ window.onload = () => {
         buttonIcon: 'ya',
       }
     )
+      .then(({ handler }) => handler())
       .then(async (data) => {
-        const result = await fetchYandexData(data.access_token); // здесь создаётся переменная с именем result, в которую сохраняется сам OAuth-токен, который мы получаем в результате работы вставленного нами скрипта
-        authorize(result); // здесь мы авторизуем пользователя с сохранённым в result OAuth-токеном
-        console.log(result, data); // #а здесь сохраняем запись токена авторизации и текущей даты в логи, чтобы потом можно было проанализировать работу, если возникла ошибка
+        const result = await fetchYandexData(data.access_token);
+        authorize(result);
+        console.log(result, data);
       })
-      .then((data) => console.log("Сообщение с токеном", data))
-      .catch((error) => console.log("Обработка ошибки", error));
+      .catch((error) => console.log("Что-то пошло не так: ", error));
   };
 };
