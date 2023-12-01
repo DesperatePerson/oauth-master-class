baef874b253b0962ba6462cc39583494790be26f
Vladislav Ivanov <103201559+DesperatePerson@users.noreply.github.com>
2023-12-01 14:41:40 +0500

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,3 +1,15 @@
 window.onload = () => {
-  // TODO
+  YaAuthSuggest.init(
+      {
+         client_id: 'c46f0c53093440c39f12eff95a9f2f93',
+         response_type: 'token',
+         redirect_uri: 'https://examplesite.com/suggest/token'
+      },
+      'https://examplesite.com'
+   )
+   .then(({
+      handler
+   }) => handler())
+   .then(data => console.log('Сообщение с токеном', data))
+   .catch(error => console.log('Обработка ошибки', error));
 };
