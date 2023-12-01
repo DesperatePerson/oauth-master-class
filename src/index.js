9c79325d7aeeca6379986694a8b1bdd40c3fb85e
Vladislav Ivanov <103201559+DesperatePerson@users.noreply.github.com>
2023-12-01 15:53:35 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  document.getElementById("button").onclick = () => {
+  document.getElementById("suggest").onclick = () => {
     YaAuthSuggest.init(
       {
          client_id: '07ba81568e504583b5f33282cfdcd5b1',
@@ -7,30 +7,30 @@ window.onload = () => {
          redirect_uri: 'https://oauth-master-class-azure.vercel.app/token.html'
       },
       'https://oauth-master-class-azure.vercel.app/'
-   )
-   // .then(({
-   //    handler
-   // }) => handler())
-   // .then(data => console.log('Сообщение с токеном', data))
-   // .catch(error => console.log('Обработка ошибки', error));
+   )  
+      .then(({ handler }) => handler())
+      .then((data) => console.log("Сообщение с токеном", data))
+      .catch((error) => console.log("Обработка ошибки", error));
+  };
 
-   //  window.YaAuthSuggest.init(
-   //    {
-   //      client_id: "",
-   //      response_type: "token",
-   //      redirect_uri: "",
-   //    },
-   //    "",
-   //    {
-   //      view: "button",
-   //      parentId: "buttonContainerId",
-   //      buttonSize: 'm',
-   //      buttonView: 'main',
-   //      buttonTheme: 'light',
-   //      buttonBorderRadius: "22",
-   //      buttonIcon: 'ya',
-   //    }
-   //  )
+  document.getElementById("button").onclick = () => {
+    window.YaAuthSuggest.init(
+      {
+        client_id: "07ba81568e504583b5f33282cfdcd5b1",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class-azure.vercel.app/token.html",
+      },
+      "https://oauth-master-class-azure.vercel.app/",
+      {
+        view: "button",
+        parentId: "buttonContainerId",
+        buttonSize: 'm',
+        buttonView: 'main',
+        buttonTheme: 'light',
+        buttonBorderRadius: "22",
+        buttonIcon: 'ya',
+      }
+    )
       .then(({ handler }) => handler())
       .then((data) => console.log("Сообщение с токеном", data))
       .catch((error) => console.log("Обработка ошибки", error));
