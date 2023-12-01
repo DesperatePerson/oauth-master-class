5e76827191870f9ccc2b185e5966b68d3ae16377
Vladislav Ivanov <103201559+DesperatePerson@users.noreply.github.com>
2023-12-01 15:47:58 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 window.onload = () => {
   document.getElementById("button").onclick = () => {
-    window.YaAuthSuggest.init(
+    YaAuthSuggest.init(
       {
-        client_id: "07ba81568e504583b5f33282cfdcd5b1",
-        response_type: "token",
-        redirect_uri: "https://oauth-master-class-azure.vercel.app/token.html",
+         client_id: '07ba81568e504583b5f33282cfdcd5b1',
+         response_type: 'token',
+         redirect_uri: 'https://oauth-master-class-azure.vercel.app/token.html'
       },
-      "https://oauth-master-class-azure.vercel.app/",
-      {
-        view: "button",
-        parentId: "buttonContainerId",
-        buttonSize: 'm',
-        buttonView: 'main',
-        buttonTheme: 'light',
-        buttonBorderRadius: "22",
-        buttonIcon: 'ya',
-      }
-    )
+      'https://oauth-master-class-azure.vercel.app/'
+   )
+   // .then(({
+   //    handler
+   // }) => handler())
+   // .then(data => console.log('Сообщение с токеном', data))
+   // .catch(error => console.log('Обработка ошибки', error));
+
+   //  window.YaAuthSuggest.init(
+   //    {
+   //      client_id: "",
+   //      response_type: "token",
+   //      redirect_uri: "",
+   //    },
+   //    "",
+   //    {
+   //      view: "button",
+   //      parentId: "buttonContainerId",
+   //      buttonSize: 'm',
+   //      buttonView: 'main',
+   //      buttonTheme: 'light',
+   //      buttonBorderRadius: "22",
+   //      buttonIcon: 'ya',
+   //    }
+   //  )
       .then(({ handler }) => handler())
       .then((data) => console.log("Сообщение с токеном", data))
       .catch((error) => console.log("Обработка ошибки", error));
